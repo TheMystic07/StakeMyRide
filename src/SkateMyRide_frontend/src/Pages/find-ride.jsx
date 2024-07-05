092f3bd15399a580dc36927fe0202ead94fecf06
Aditya <singhadi437@gmail.com>
2024-07-06 03:06:14 +0530

    rides data being fetched and deplayed correctly now

--- src/SkateMyRide_frontend/src/Pages/find-ride.jsx
+++ src/SkateMyRide_frontend/src/Pages/find-ride.jsx
@@ -6,7 +6,7 @@ import { db } from "../Firebase";
 import { collection, getDocs } from "firebase/firestore";
 
 const FindRidePage = () => {
-  const [offeredRides, setOfferedRides] = useState({});
+  const [offeredRides, setOfferedRides] = useState([]);
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const pickupLocation = queryParams.get("pickup");
@@ -15,28 +15,24 @@ const FindRidePage = () => {
   useEffect(() => {
     const fetchOfferedRides = async () => {
       const querySnapshot = await getDocs(collection(db, "offeredRides"));
-      const rides = querySnapshot.docs.map((doc) => doc.data());
+      const rides = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+        departureDateTime: doc.data().departureDateTime.toDate(),
+        arrivalDateTime: doc.data().arrivalDateTime.toDate(),
+      }));
+
       console.log(rides);
 
-      const filteredRides = {};
-
-      rides.forEach((ride) => {
-        if (
+      const filteredRides = rides.filter(
+        (ride) =>
           ride.pickupLocation &&
           ride.dropoffLocation &&
           pickupLocation &&
           destinationLocation &&
           ride.pickupLocation.toLowerCase() === pickupLocation.toLowerCase() &&
-          ride.dropoffLocation.toLowerCase() ===
-            destinationLocation.toLowerCase()
-        ) {
-          const key = `${ride.pickupLocation}-${ride.dropoffLocation}`;
-          if (!filteredRides[key]) {
-            filteredRides[key] = [];
-          }
-          filteredRides[key].push(ride);
-        }
-      });
+          ride.dropoffLocation.toLowerCase() === destinationLocation.toLowerCase()
+      );
 
       setOfferedRides(filteredRides);
     };
@@ -48,40 +44,26 @@ const FindRidePage = () => {
     <div className="container">
       <div className="sidebar">
         <h1>Find a ride</h1>
-        {Object.keys(offeredRides).length > 0 &&
-          Object.keys(offeredRides).map((key) => {
-            const [pickup, destination] = key.split("-");
-            return (
-              <>
-                <p>From {pickup} to {destination}</p>
-                {offeredRides[key].map((ride, index) => (
-                  <div className="location" key={index}>
-                    <FaMapMarkerAlt className="icon" />
-                    <div>
-                      <h2>{ride.pickupLocation}</h2>
-                      <p className="time">
-                        Leaving{" "}
-                        {ride.departureDateTime.toDate().toLocaleString()}
-                      </p>
-                    </div>
-                  </div>
-                ))}
-                <h1>TO</h1>
-                {offeredRides[key].map((ride, index) => (
-                  <div className="location" key={index}>
-                    <FaMapMarkerAlt className="icon" />
-                    <div>
-                      <h2>{ride.dropoffLocation}</h2>
-                      <p className="time">
-                        Arriving{" "}
-                        {ride.arrivalDateTime.toDate().toLocaleString()}
-                      </p>
-                    </div>
-                  </div>
-                ))}
-              </>
-            );
-          })}
+        {offeredRides.length > 0 ? (
+          <>
+            <p>From {pickupLocation} to {destinationLocation}</p>
+            <div className="location">
+              <FaMapMarkerAlt className="icon" />
+              <div>
+                <h2>{pickupLocation}</h2>
+              </div>
+            </div>
+            <h1>TO</h1>
+            <div className="location">
+              <FaMapMarkerAlt className="icon" />
+              <div>
+                <h2>{destinationLocation}</h2>
+              </div>
+            </div>
+          </>
+        ) : (
+          <p>No rides available for the requested locations!</p>
+        )}
       </div>
 
       <div className="main">
@@ -101,9 +83,9 @@ const FindRidePage = () => {
         </div>
 
         <div className="rides">
-          {Object.keys(offeredRides).length > 0 ? (
-            Object.values(offeredRides).flat().map((ride, index) => (
-              <div className="ride" key={index}>
+          {offeredRides.length > 0 ? (
+            offeredRides.map((ride, index) => (
+              <div className="ride" key={ride.id}>
                 <div className="profile-picture">
                   <img
                     src={`https://randomuser.me/api/portraits/thumb/men/${index}.jpg`}
@@ -112,16 +94,9 @@ const FindRidePage = () => {
                 </div>
                 <div className="ride-info">
                   <h4>{ride.Name}</h4>
-                  <p>
-                    {ride.departureDateTime.toDate().toLocaleString()} -{" "}
-                    {ride.arrivalDateTime.toDate().toLocaleString()}
-                  </p>
-                  <p>
-                    {ride.pickupLocation} - {ride.dropoffLocation}
-                  </p>
-                  <p>
-                    Car: {ride.make} {ride.model} {ride.year} {ride.color}
-                  </p>
+                  <p>{ride.departureDateTime.toLocaleString()} - {ride.arrivalDateTime.toLocaleString()}</p>
+                  <p>{ride.pickupLocation} - {ride.dropoffLocation}</p>
+                  <p>Car: {ride.make} {ride.model} {ride.year} {ride.color}</p>
                   <p>License Plate: {ride.licensePlate}</p>
                   <p>Number of Seats: {ride.numberOfSeats}</p>
                   <p>Price per Seat: {ride.pricePerSeat}</p>
